760e7406daa85d0d6f9b9e152a8117f51ab35926
root <root@192.168.1.18>
2021-05-28 01:08:39 +0900

    Pembaruan index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
  
 app.get('/', (req, res) => {
-   res.send('<h1>Holaaa </h1>')
+   res.send('<h1>Holaaa Testt</h1>')
 })
  
 const PORT = process.env.PORT || 3000
