762eaa766fef21ce6a9720c03e4b481129a421f4
dedysamhaz <dedysamhaz@gmail.com>
2021-06-09 14:51:19 +0900

    testtt

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send('Hello Ddy');
+  res.send('Hello Deedy');
 });
 
 app.listen(PORT, HOST);
